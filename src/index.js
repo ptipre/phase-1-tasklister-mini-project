aec10eb1b4664c11b5737e839dbbaacacb697486
Prajay Tipre <prajaytipre@gmail.com>
2021-11-09 11:23:26 -0500

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector('form');
+  form.addEventListener('submit', buildTask);
 });
+
+function buildTask(e) {
+  e.preventDefault();
+  let form = document.querySelector('form')
+  let userInput = document.getElementById('new-task-description').value
+  let taskList = document.getElementById('tasks')
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+
+  btn.addEventListener('click',  e => {
+    let parent = e.target.parentNode;
+    parent.remove();
+  })
+
+  li.textContent = `${userInput}   `;
+  btn.textContent = 'x'
+
+  li.append(btn)
+  taskList.appendChild(li);
+  form.reset();
+}
+
+
